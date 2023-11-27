c9db4dc24e5000fc02624c6a545d8a9e24684009
montes767 <151910016+montes767@users.noreply.github.com>
2023-11-27 11:03:25 +0100

    Update ClasePrincipal.java

--- ejemploGit/src/main/java/paqueteprincipal/ClasePrincipal.java
+++ ejemploGit/src/main/java/paqueteprincipal/ClasePrincipal.java
@@ -5,6 +5,11 @@ public class ClasePrincipal {
 	public static void main(String[] args) {
 		System.out.println("commit de github");
 
+		String mivariable;
+
+
+		Srting mivariable="definifaEnGithub";
+
 	}
 
 }
